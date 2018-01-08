0b04718cb4e099f033fc1d48929d7ed109dda4d5
Mateusz Dobek <mat.dobek@gmail.com>
2018-01-08 18:26:06 +0100

    Update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 scp -r ./assets/ r_priv:/var/www/matdobek.com/assets/
-scp ./index.html r_priv:/var/www/matdobek.com/index.html
+scp ./index.html r_priv:/var/www/matdobek.com/
+scp ./pgp.html r_priv:/var/www/matdobek.com/
